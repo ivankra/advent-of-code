0dace721e0a4e00822237a7679e458f96e493e0e
ivankra <175570+ivankra@users.noreply.github.com>
2022-12-05 16:28:55 +0800

    https://adventofcode.com/2022/day/5

---
+++ 2022/05/p1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import re
+
+drawing = []
+for line in sys.stdin:
+    line = line.rstrip()
+    if line == '':
+        break
+    else:
+        drawing.append(line)
+
+N = int(drawing.pop().split()[-1])
+stacks = []
+for i in range(N):
+    stacks.append([])
+    for line in drawing:
+        j = 1+i*4
+        if j < len(line) and line[j] != ' ':
+            stacks[i].append(line[j])
+    stacks[i] = list(reversed(stacks[i]))
+
+for line in sys.stdin:
+    m = re.fullmatch('move ([0-9]+) from ([0-9]) to ([0-9])$', line.strip())
+    assert m, line
+    k = int(m[1])
+    i = int(m[2])
+    j = int(m[3])
+    for _ in range(k):
+        stacks[j-1].append(stacks[i-1].pop())
+
+print(''.join(s[-1] for s in stacks))
