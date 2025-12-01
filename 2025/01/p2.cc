1a1f8d6fd8faef515bd068a202a359a4e2dc2ea7
Ivan Krasilnikov <ivank@ivank.me>
2025-12-01 13:15:38 +0800

    2025/01

---
+++ 2025/01/p2.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+int main() {
+  char dir;
+  int n, cur = 50, ans = 0;
+
+  while (scanf(" %c%d", &dir, &n) == 2) {
+    for (int i = 0; i < n; i++) {
+      if (dir == 'L') cur--; else cur++;
+      cur = (cur + 100) % 100;
+      if (cur == 0) ans++;
+    }
+  }
+
+  printf("%d\n", ans);
+}
